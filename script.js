f23326a975c0a1e48b131f336b7ccdee8aa385cf
flavinpv <flvsadr@gmail.com>
2022-05-24 14:53:28 -0300

    criando, inserindo e editando a tarefa

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
 
+
 function addTask(button){
     // get the parent element
     let buttonParent = button.parentNode.parentNode;
     // get the div to make and store the new task
     let currentChecklist = buttonParent.querySelector(".checklist");
     let textInput = buttonParent.querySelector(".taskInput");
+
+    if (!textInput.value){
+        // disable button when input is empty
+        console.log(button)
+        textInput.setAttribute("placeholder", "input required")
+
+        return 0;
+    }
     
     let newText = textInput.value
-    console.log(newText)
+    // console.log(newText)
     
     // create the new checkbox element
     newCheckbox = document.createElement("input")
     newCheckbox.setAttribute("type","checkbox")
-    newCheckbox.setAttribute("id", `label${countid}`)
+    // newCheckbox.setAttribute("id", `label${countid}`)
 
     // make a label for this checkbox
     newLabel = document.createElement("label")
-    newLabel.setAttribute("for", `label${countid}`)
+    // newLabel.setAttribute("for", `label${countid}`)
+    // make the text content editable
+    newLabel.setAttribute("contentEditable","true")
     newLabel.innerText = newText
 
+    //make a new div to group the label and the checkbox input
    checkboxGroup = document.createElement('div')
    checkboxGroup.setAttribute("class","checkboxGroup")
 
 
-    console.log(newLabel)
+    // console.log(newLabel)
 
     checkboxGroup.appendChild(newCheckbox)
     checkboxGroup.appendChild(newLabel)
 
-    console.log(newCheckbox)
+    // console.log(newCheckbox)
     // put the new elements into checklist div
     currentChecklist.appendChild(checkboxGroup)
 
-    console.log(currentChecklist.firstChild)
+    console.log(currentChecklist)
     // update the id counter
     countid++
+
+    // reset the input text
+    textInput.value = ""
+
 
 }
 //store the id counter
 let countid = 1
+
+function inputRequired(ipt){
+    console.log(ipt)
+
+}
